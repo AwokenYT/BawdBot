0e043e028e5b6ae640d216f40ec67e8db7133a96
AwokenYT <88662772+AwokenYT@users.noreply.github.com>
2022-06-29 18:55:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 	
 		prefix: "-", // required, command prefix
-		botToken: "OTM3Mjg4NjQwOTY1NTI1NTk2.G_A_Sd.-YX2WLCRnQPGenEW01vTKMGDk8vc01gDs3a1_E", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+		botToken: "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
 		clientId: "937288640965525596", // optional, the bot's client ID, leave empty to disable slash commands
 		geniusApiToken: "_9i278pcsJ3MrHkTOTWJXH3X9fQ9l7uUSG3NvZfMZ_FVqiqx3o0LDxUBpJ2qwXlLqfNpQp89Yg98sOpawLWAQg", // optional, but recommended for lyrics search - https://genius.com/api-clients
 
